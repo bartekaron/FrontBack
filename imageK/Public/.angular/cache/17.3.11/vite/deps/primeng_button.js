d6a1f82cbb5769b84817b148d7b5a4b42ef18aa8
bartekaron <78853724+bartekaron@users.noreply.github.com>
2025-06-04 22:38:16 +0200

    Image

---
+++ imageK/Public/.angular/cache/17.3.11/vite/deps/primeng_button.js
@@ -0,0 +1,18 @@
+import {
+  Button,
+  ButtonDirective,
+  ButtonModule
+} from "./chunk-A6LGPYO5.js";
+import "./chunk-YAXL6MAE.js";
+import "./chunk-Q6XGPLBD.js";
+import "./chunk-YHTDT4CO.js";
+import "./chunk-ZMI6KQJX.js";
+import "./chunk-XLSINCXO.js";
+import "./chunk-YFXVGDDX.js";
+import "./chunk-W7WAD56I.js";
+export {
+  Button,
+  ButtonDirective,
+  ButtonModule
+};
+//# sourceMappingURL=primeng_button.js.map
